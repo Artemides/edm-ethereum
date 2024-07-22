db6b8b6ef7281b476435487e0114f75ef9c42ddf
Artemides <edmpulasky@gmail.com>
2024-07-22 16:50:45 -0500

    unify custom rainbow custom component

---
+++ src/components/scaffold-eth/rainbow-kit-custom-connect/index.tsx
@@ -0,0 +1,74 @@
+import { useNetworkColor } from "@/hooks/useNetworkColor";
+import { useTargetNetwork } from "@/hooks/useTargetNetwork";
+import {
+  getBlockExplorerAddressLink,
+  getTargetNetworks,
+} from "@/utils/scaffold-eth/network";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import React from "react";
+import { WrongNetworkDropdown } from "./wrong-network-dropdwon";
+import { Balance } from "../balance";
+import { Address } from "viem";
+import { AddressInfoDropdown } from "./address-infor-dropdrown";
+import { AddressQRCodeModal } from "./address-qr-code-modal";
+
+export const RainbowConnectButton = () => {
+  const networkColor = useNetworkColor();
+  const { targetNetwork } = useTargetNetwork();
+  return (
+    <ConnectButton.Custom>
+      {({ account, chain, openConnectModal, mounted }) => {
+        const connected = account && mounted && chain;
+        const blockExplorerAddressLink = account
+          ? getBlockExplorerAddressLink(targetNetwork, account.address)
+          : undefined;
+
+        return (
+          <>
+            {() => {
+              if (!connected) {
+                return (
+                  <button
+                    className="btn btn-primary btn-sm"
+                    onClick={openConnectModal}
+                    type="button"
+                  >
+                    Connect Wallet
+                  </button>
+                );
+              }
+
+              if (chain?.unsupported || chain?.id !== targetNetwork.id) {
+                return <WrongNetworkDropdown />;
+              }
+
+              return (
+                <>
+                  <div className="flex flex-col items-center mr-1">
+                    <Balance
+                      address={account.address as Address}
+                      className="min-h-0 h-auto"
+                    />
+                    <span className="text-xs" style={{ color: networkColor }}>
+                      {chain.name}
+                    </span>
+                  </div>
+                  <AddressInfoDropdown
+                    address={account.address as Address}
+                    displayName={account.displayName}
+                    ensAvatar={account.ensAvatar}
+                    blockExplorerAddressLink={blockExplorerAddressLink}
+                  />
+                  <AddressQRCodeModal
+                    address={account.address as Address}
+                    modalId="qrcode-modal"
+                  />
+                </>
+              );
+            }}
+          </>
+        );
+      }}
+    </ConnectButton.Custom>
+  );
+};
--- src/services/store/store.ts
+++ src/services/store/store.ts
@@ -3,16 +3,24 @@ import { create } from "zustand";
 import { scaffoldConfig } from "../../../scaffold.config";
 
 type GlobalState = {
-  nativeCurrencyPrice: number;
+  nativeCurrency: { price: number; isFetching: boolean };
   setNativeCurrencyPrice: (price: number) => void;
+  setNativeCurrencyFetching: (isFetching: boolean) => void;
   targetNetwork: ChainWithAttributes;
   setTargetNetwork: (network: ChainWithAttributes) => void;
 };
 
 export const useGlobalState = create<GlobalState>((set) => ({
-  nativeCurrencyPrice: 0,
+  nativeCurrency: {
+    price: 0,
+    isFetching: false,
+  },
   setNativeCurrencyPrice: (price) =>
-    set(() => ({ nativeCurrencyPrice: price })),
+    set((state) => ({ nativeCurrency: { ...state.nativeCurrency, price } })),
+  setNativeCurrencyFetching: (isFetching) =>
+    set((state) => ({
+      nativeCurrency: { ...state.nativeCurrency, isFetching },
+    })),
   targetNetwork: scaffoldConfig.targetNetworks[0],
   setTargetNetwork: (network) => set(() => ({ targetNetwork: network })),
 }));
