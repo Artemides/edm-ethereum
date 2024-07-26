80c7f2d32f64d9770479e862c8fec873c9729f26
Artemides <edmpulasky@gmail.com>
2024-07-25 19:27:01 -0500

    add ipfs api

--- src/app/mynfts/page.tsx
+++ src/app/mynfts/page.tsx
@@ -1,6 +1,19 @@
+import { useSpeedReadContract } from "@/hooks/useSpeedReadContract";
+import { useSpeedWriteContract } from "@/hooks/useSpeedWriteContract";
 import { NextPage } from "next";
 import React from "react";
+import { useAccount } from "wagmi";
 
 export const MyNFTS: NextPage = () => {
+  const { address: connectedAddress, isConnected, isConnecting } = useAccount();
+
+  const { writeContractAsync } = useSpeedWriteContract("YourCollectible");
+
+  const { data: tokenIdCounter } = useSpeedReadContract({
+    contractName: "YourCollectible",
+    functionName: "tokenIdCounter",
+    watch: true,
+  });
+
   return <div>MyNFTS</div>;
 };
