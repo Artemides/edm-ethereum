b41b6c30670b3c9ea2adeea611bf2bddaccd2261
Artemides <edmpulasky@gmail.com>
2024-08-07 12:03:36 -0500

    make header app logo fixed size

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -211,11 +211,12 @@ export const Header = () => {
           passHref
           className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0"
         >
-          <div className="flex relative w-10 h-10">
+          <div className="flex relative">
             <Image
               alt="SE2 logo"
               className="cursor-pointer"
-              fill
+              width={40}
+              height={40}
               src="/favicon.png"
             />
           </div>
