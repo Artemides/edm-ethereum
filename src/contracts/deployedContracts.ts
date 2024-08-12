9612b817dda26be62118618a833e57d0c8da4590
Artemides <edmpulasky@gmail.com>
2024-08-12 05:38:29 -0500

    render dex page

--- src/contracts/deployedContracts.ts
+++ src/contracts/deployedContracts.ts
@@ -7,8 +7,8 @@ import { GenericContractsDeclaration } from "@/utils/scaffold-eth/contract";
 
 const deployedContracts = {
   31337: {
-    Ballons: {
-      address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    Balloons: {
+      address: "0xa85233C63b9Ee964Add6F2cffe00Fd84eb32338f",
       abi: [
         {
           inputs: [],
@@ -301,7 +301,7 @@ const deployedContracts = {
       },
     },
     DEX: {
-      address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+      address: "0x4A679253410272dd5232B3Ff7cF5dbB88f295319",
       abi: [
         {
           inputs: [
@@ -579,7 +579,7 @@ const deployedContracts = {
       inheritedFunctions: {},
     },
     DiceGame: {
-      address: "0x610178dA211FEF7D417bC0e6FeD39F05609AD788",
+      address: "0x4ed7c70F96B99c776995fB64377f0d4aB3B0e1C1",
       abi: [
         {
           inputs: [],
@@ -676,7 +676,7 @@ const deployedContracts = {
       inheritedFunctions: {},
     },
     ExampleExternalContract: {
-      address: "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6",
+      address: "0xc6e7DF5E7b4f2A278906862b61205850344D4e7d",
       abi: [
         {
           inputs: [],
@@ -702,7 +702,7 @@ const deployedContracts = {
       inheritedFunctions: {},
     },
     RiggedRoll: {
-      address: "0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e",
+      address: "0x322813Fd9A801c5507c9de605d63CEA4f2CE6c44",
       abi: [
         {
           inputs: [
@@ -817,7 +817,7 @@ const deployedContracts = {
       },
     },
     SomeContract: {
-      address: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
+      address: "0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1",
       abi: [
         {
           inputs: [],
@@ -854,7 +854,7 @@ const deployedContracts = {
       inheritedFunctions: {},
     },
     Staker: {
-      address: "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318",
+      address: "0x59b670e9fA9D0A427751Af201D676719a970857b",
       abi: [
         {
           inputs: [
@@ -1059,7 +1059,7 @@ const deployedContracts = {
       inheritedFunctions: {},
     },
     StakerEpochs: {
-      address: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
+      address: "0x9A9f2CCfdE556A7E9Ff0848998Aa4a0CFD8863AE",
       abi: [
         {
           inputs: [
@@ -1414,7 +1414,7 @@ const deployedContracts = {
       inheritedFunctions: {},
     },
     Vendor: {
-      address: "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853",
+      address: "0x3Aa5ebB10DC797CAC828524e59A333d0A371443c",
       abi: [
         {
           inputs: [
@@ -1594,7 +1594,7 @@ const deployedContracts = {
       },
     },
     YourCollectible: {
-      address: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+      address: "0x0B306BF915C4d645ff596e518fAf3F9669b97016",
       abi: [
         {
           inputs: [],
@@ -2090,7 +2090,7 @@ const deployedContracts = {
       },
     },
     YourContract: {
-      address: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+      address: "0x9A676e781A523b5d0C0e43731313A708CB607508",
       abi: [
         {
           inputs: [
@@ -2233,7 +2233,7 @@ const deployedContracts = {
       inheritedFunctions: {},
     },
     YourToken: {
-      address: "0x0165878A594ca255338adfa4d48449f69242Eb8F",
+      address: "0x68B1D87F95878fE05B998F19b66F4baba5De1aed",
       abi: [
         {
           inputs: [],
