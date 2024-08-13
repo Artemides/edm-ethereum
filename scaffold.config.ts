e22672788826fc30b07e177092a56948c3e82d15
Artemides <edmpulasky@gmail.com>
2024-08-13 15:36:37 -0500

    deploy contract to sepolia

--- scaffold.config.ts
+++ scaffold.config.ts
@@ -11,15 +11,13 @@ const isDevelopment = process.env.NODE_ENV === "development";
 
 export const scaffoldConfig = {
   targetNetworks: [
-    chains.hardhat,
+    // chains.hardhat,
     chains.sepolia,
     chains.arbitrum,
-    // ...(isDevelopment ? [chains.hardhat] : []),
+    ...(isDevelopment ? [chains.hardhat] : []),
   ],
   pollingInterval: 30000,
   alchemyApiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || "",
-  walletConnectProjId:
-    process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID ||
-    "3a8170812b534d0ff9d794f19a901d64",
+  walletConnectProjId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "3a8170812b534d0ff9d794f19a901d64",
   onlyLocalBurnerWallet: true,
 } as const satisfies ScaffoldConfig;
