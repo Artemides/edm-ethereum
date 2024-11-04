6ab5ed5716609e77fef646de96b180d8122fc76a
Edmundo Arias <Edmpulasky@gmail.com>
2024-11-04 00:41:22 -0500

    create orbit component

---
+++ src/components/portfolio/orbit.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { cn } from "@/utils/ui";
+import React, { createRef, useEffect, useRef, useState } from "react";
+
+type OrbitProps = {
+  className?: string;
+  elements: React.ReactNode[];
+};
+
+export const Orbit = ({ elements, className }: OrbitProps) => {
+  const [orbitDim, setOrbitDim] = useState({ width: 0, height: 0 });
+  const orbitRef = useRef<HTMLDivElement>(null);
+  const refs = elements.map(() => createRef<HTMLDivElement>());
+
+  useEffect(() => {
+    if (!orbitRef.current) return;
+
+    setOrbitDim({ width: orbitRef.current.clientWidth, height: orbitRef.current.clientHeight });
+  }, []);
+
+  return (
+    <div
+      ref={orbitRef}
+      className={cn("m-auto relative w-[100px] aspect-square border-[1px] border-white/5  rounded-full ", className)}
+    >
+      {elements.map((el, i) => {
+        if (!orbitRef.current) return null;
+
+        let angle = (360 / elements.length) * i;
+        const radius = orbitDim.width / 2;
+        const top = radius + radius * Math.sin((angle * Math.PI) / 180) - (refs[i].current?.clientHeight ?? 0) / 2;
+        const left = radius + radius * Math.cos((angle * Math.PI) / 180) - (refs[i].current?.clientWidth ?? 0) / 2;
+        return (
+          <div
+            ref={refs[i]}
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            style={{ top, left }}
+          >
+            {el}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
--- src/components/portfolio/universe.tsx
+++ src/components/portfolio/universe.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Bubble } from "./bubble";
 import Image from "next/image";
+import { Orbit } from "./orbit";
 
 const topics = [
   {
@@ -55,51 +56,58 @@ const topics = [
 ];
 
 export const Universe = () => {
+  const orbitElements = topics.map((topic, i) => {
+    return (
+      <div className="">
+        <Bubble
+          id={`border-gradient-${i}`}
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-20 aspect-square "
+          bColor={topic.bColor}
+          bgColor={topic.bgColor}
+        >
+          <span className="absolute text-[40px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">{topic.icon}</span>
+        </Bubble>
+        <p
+          style={{ backgroundColor: topic.bgColor2, borderColor: topic.bColor }}
+          className="mt-36 px-8 py-1 rounded-full border-[1px]"
+        >
+          {topic.title}
+        </p>
+      </div>
+    );
+  });
+
   return (
     <div id="universe" className="mb-24">
-      <div className="m-auto relative w-[450px] aspect-square border-[1px] border-white/5  rounded-full estellar-cloud-gradient ">
-        <div className="absolute w-2/3 aspect-square top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  border-[1px] border-white/5  rounded-full"></div>
-
-        <div className="absolute w-1/3 aspect-square top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  border-[1px] border-white/5  rounded-full"></div>
-
+      <div className="m-auto relative w-[450px] aspect-square ">
         <div className="absolute w-[1px] aspect-square top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 blured-shadow"></div>
+        <Orbit className="absolute w-full estellar-cloud-gradient z-10" elements={orbitElements} />
+
         <Image
           src={"/images/portfolio/ether.webp"}
           alt="ether"
           width={130}
           height={200}
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 "
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  z-10"
         />
-        {/* 
-            a = (360 / 7) * i 
-            x = (cx+ radius) * cos(a)  - half \
-            y = (cy+ radius) * sin(a)  - half *
-        */}
-        {topics.map((topic, i) => {
-          let alpha = (360 / topics.length) * i;
-          let x = 225 - 65 + (450 / 2) * Math.cos((alpha * Math.PI) / 180);
-          let y = 100 + 20 + (450 / 2) * Math.sin((alpha * Math.PI) / 180);
-          return (
-            <div style={{ left: `${x}px`, top: `${y}px` }} className="absolute">
-              <Bubble
-                id={`border-gradient-${i}`}
-                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-20 aspect-square "
-                bColor={topic.bColor}
-                bgColor={topic.bgColor}
-              >
-                <span className="absolute text-[40px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
-                  {topic.icon}
-                </span>
-              </Bubble>
-              <p
-                style={{ backgroundColor: topic.bgColor2, borderColor: topic.bColor }}
-                className="mt-36 px-8 py-1 rounded-full border-[1px]"
-              >
-                {topic.title}
-              </p>
-            </div>
-          );
-        })}
+        <Orbit
+          className="absolute w-full aspect-square top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  border-[1px] border-white/5  rounded-full -z-1"
+          elements={new Array(5).fill(null).map((_, idx) => (
+            <div key={`orbit-element-${idx}`} className="w-3 h-3 rounded-full bg-[#2A2C3E]"></div>
+          ))}
+        />
+        <Orbit
+          className="absolute w-2/3 aspect-square top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  border-[1px] border-white/5  rounded-full -z-1"
+          elements={new Array(3).fill(null).map((_, idx) => (
+            <div key={`orbit-element-${idx}`} className="w-3 h-3 rounded-full bg-[#2A2C3E]"></div>
+          ))}
+        />
+        <Orbit
+          className="absolute w-1/3 aspect-square top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  border-[1px] border-white/5  rounded-full -z-1"
+          elements={new Array(2).fill(null).map((_, idx) => (
+            <div key={`orbit-element-${idx}`} className="w-3 h-3  rounded-full bg-[#2A2C3E]"></div>
+          ))}
+        />
       </div>
     </div>
   );
