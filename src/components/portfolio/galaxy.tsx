4d78045623bc42c890ab13de4d94f87f0459ff21
Edmundo Arias <Edmpulasky@gmail.com>
2024-11-12 19:22:19 -0500

    enhance responsiveness for section develop

--- src/components/portfolio/galaxy.tsx
+++ src/components/portfolio/galaxy.tsx
@@ -24,7 +24,12 @@ export const Galaxy = () => {
           rotateElements={process.env.NODE_ENV === "production"}
         />
 
-        <div className="absolute-center size-1/3  z-10 orbit-eye-bounce">
+        <div
+          className={cn(
+            "absolute-center size-1/3  z-10 ",
+            process.env.NODE_ENV === "production" ? "orbit-eye-bounce" : ""
+          )}
+        >
           <Image
             src={"/images/portfolio/ether.webp"}
             alt="ether"
@@ -34,19 +39,28 @@ export const Galaxy = () => {
           />
         </div>
         <Orbit
-          className="absolute w-full aspect-square top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  border-[1px] border-white/5  rounded-full -z-1  animate-rotate-slow"
+          className={cn(
+            "absolute w-full aspect-square top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  border-[1px] border-white/5  rounded-full -z-1  ",
+            process.env.NODE_ENV === "production" ? "animate-rotate-slow" : ""
+          )}
           elements={new Array(5).fill(null).map((_, idx) => (
             <div key={`orbit-element-${idx}`} className="w-3 h-3 rounded-full bg-[#2A2C3E]"></div>
           ))}
         />
         <Orbit
-          className="absolute w-2/3 aspect-square top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  border-[1px] border-white/5  rounded-full -z-1 animate-orbit-reverse-origin"
+          className={cn(
+            "absolute w-2/3 aspect-square top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  border-[1px] border-white/5  rounded-full -z-1 ",
+            process.env.NODE_ENV === "production" ? "animate-orbit-reverse-origin" : ""
+          )}
           elements={new Array(3).fill(null).map((_, idx) => (
             <div key={`orbit-element-${idx}`} className="w-3 h-3 rounded-full bg-[#2A2C3E]"></div>
           ))}
         />
         <Orbit
-          className="absolute w-1/3 aspect-square top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  border-[1px] border-white/5  rounded-full -z-1 animate-rotate-fast"
+          className={cn(
+            "absolute w-1/3 aspect-square top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  border-[1px] border-white/5  rounded-full -z-1 ",
+            process.env.NODE_ENV === "production" ? "animate-rotate-fast" : ""
+          )}
           elements={new Array(3).fill(null).map((_, idx) => (
             <div key={`orbit-element-${idx}`} className="w-3 h-3 rounded-full bg-[#2a2c3e]"></div>
           ))}
